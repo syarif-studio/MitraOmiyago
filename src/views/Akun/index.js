c175b76a3fddb7c8833ceaae26211209f0b4daa3
Syarif <syarifuddin.studio@gmail.com>
2021-01-05 14:05:22 +0800

    Update akun

--- src/views/Akun/index.js
+++ src/views/Akun/index.js
@@ -1,133 +1,194 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
 import { View, TouchableOpacity, StyleSheet, AsyncStorage } from 'react-native';
-import { Container, Header, Content, Card, CardItem, Thumbnail, Text, Button, Icon, Left, Body, Right, Title, Footer, FooterTab} from 'native-base';
-import { Item, Input, Grid, Row, ActionSheet, ImageBackground } from 'native-base';
-import { removeData, retrieveData } from "../../services/storage"
-import { StackActions, NavigationActions  } from 'react-navigation';
-
+import {
+  Container,
+  Header,
+  Content,
+  Card,
+  CardItem,
+  Thumbnail,
+  Text,
+  Button,
+  Icon,
+  Left,
+  Body,
+  Right,
+  Title,
+  Footer,
+  FooterTab,
+} from 'native-base';
+import { removeData, retrieveData } from '../../services/storage';
+import { StackActions, NavigationActions } from 'react-navigation';
 
 var BUTTONS = [
-  { text: "Beranda", icon: "home", iconColor: "#2c8ef4" },
-  { text: "Kategori", icon: "analytics", iconColor: "#f42ced" },
-  { text: "Promo", icon: "gift", iconColor: "#ea943b" },
-  { text: "Bantuan", icon: "cog", iconColor: "#ea943b" },
-  { text: "Logout", icon: "close", iconColor: "#fa213b" },
-  
-
+  { text: 'Beranda', icon: 'home', iconColor: '#2c8ef4' },
+  { text: 'Kategori', icon: 'analytics', iconColor: '#f42ced' },
+  { text: 'Promo', icon: 'gift', iconColor: '#ea943b' },
+  { text: 'Bantuan', icon: 'cog', iconColor: '#ea943b' },
+  { text: 'Logout', icon: 'close', iconColor: '#fa213b' },
 ];
 var DESTRUCTIVE_INDEX = 3;
 var CANCEL_INDEX = 4;
 
 export default class CardImageExample extends Component {
-
   state = {
-    userData: null
-  }
+    userData: null,
+  };
 
   logoutHandler = () => {
     removeData('userData');
     const resetAction = StackActions.reset({
       index: 0,
-      actions: [NavigationActions.navigate({ routeName: 'BottomNavigation' })],
+      actions: [NavigationActions.navigate({ routeName: 'Login' })],
     });
     this.props.navigation.dispatch(resetAction);
     //this.props.navigation.navigate('Home')
+  };
+
+  async componentDidMount() {
+    const user = await retrieveData('userData');
+    this.setState({ userData: user });
   }
 
-  async componentDidMount(){
-    const user = await retrieveData('userData');
-    this.setState({userData: user});
-  }
-  
   render() {
     const user = this.state.userData;
     if (!user) return null;
 
     return (
-      
       <Container>
-       
+        <View
+          style={{
+            backgroundColor: '#FFF',
+            marginTop: 20,
+            padding: 16,
+            elevation: 1,
+          }}>
+          <Title style={{ color: '#333', alignSelf: 'center' }}>
+            Akun Saya
+          </Title>
+        </View>
         <Content>
-          
-          <Card style={{marginTop: 20}}>
-            <CardItem>
-              <Left>
-              <Image source={require('../../assets/img/user2.png')} style={{height:70, width:70}} />
-                <Body>
-                  <Text>{user.name}</Text>
-                  <Text>{user.email}</Text>
-                  
-                </Body>
-              </Left>
-            </CardItem>
-            
-          </Card>
+          <View style={{ backgroundColor: '#00D100', padding: 16 }}>
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text>Nama</Text>
+              <Text>{user.name}</Text>
+            </View>
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text>Email</Text>
+              <Text>{user.email}</Text>
+            </View>
+            <View
+              style={{ flexDirection: 'row', justifyContent: 'space-between' }}>
+              <Text>Telpon</Text>
+              <Text>{user.phone}</Text>
+            </View>
+          </View>
           <View style={styles.Main}>
-             <TouchableOpacity style={styles.Frow} onPress={() => this.props.navigation.navigate('RiwayatBelanja')}>
-                <View style={styles.SubMain}>
-                    
-                    <View style={styles.TPromo}>
-                        <Text style={styles.TText}>Transaksi</Text>
-                        <Text style={{fontSize:13}}>Semua Daftar Riwayat Transaksi</Text>
-                    </View>
+            <TouchableOpacity
+              style={styles.Frow}
+              onPress={() => this.props.navigation.navigate('RiwayatBelanja')}>
+              <View style={styles.SubMain}>
+                <View style={styles.TPromo}>
+                  <Text style={styles.TText}>Transaksi</Text>
+                  <Text style={{ fontSize: 13 }}>
+                    Semua Daftar Riwayat Transaksi
+                  </Text>
                 </View>
-                <View style={styles.arr}>
-                <Image source={require('../../assets/img/next.png')} style={styles.imgArr} />
-                </View>
+              </View>
+              <View style={styles.arr}>
+                <Image
+                  source={require('../../assets/img/next.png')}
+                  style={styles.imgArr}
+                />
+              </View>
             </TouchableOpacity>
-            
-        </View>
-        <View style={styles.Main}>
-             <TouchableOpacity style={styles.Frow} onPress={() => this.props.navigation.navigate('Wishlist')}>
-                <View style={styles.SubMain}>
-                    
-                    <View style={styles.TPromo}>
-                        <Text style={styles.TText}>Wishlist</Text>
-                        <Text style={{fontSize:13}}>Lihat Produk yang sudah Anda Wishlist</Text>
-                    </View>
+          </View>
+          <View style={styles.Main}>
+            <TouchableOpacity
+              style={styles.Frow}
+              onPress={() => this.props.navigation.navigate('Wishlist')}>
+              <View style={styles.SubMain}>
+                <View style={styles.TPromo}>
+                  <Text style={styles.TText}>Wishlist</Text>
+                  <Text style={{ fontSize: 13 }}>
+                    Lihat Produk yang sudah Anda Wishlist
+                  </Text>
                 </View>
-                <View style={styles.arr}>
-                <Image source={require('../../assets/img/next.png')} style={styles.imgArr} />
-                </View>
+              </View>
+              <View style={styles.arr}>
+                <Image
+                  source={require('../../assets/img/next.png')}
+                  style={styles.imgArr}
+                />
+              </View>
             </TouchableOpacity>
-            
-        </View>
-        <View style={styles.Main}>
-             <TouchableOpacity style={styles.Frow} onPress={() => this.props.navigation.navigate('Bantuan')}>
-                <View style={styles.SubMain}>
-                    
-                    <View style={styles.TPromo}>
-                        <Text style={styles.TText}>Lacak Pesanan</Text>
-                        <Text style={{fontSize:13}}>Melacak Order pesananmu</Text>
-                    </View>
+          </View>
+          <View style={styles.Main}>
+            <TouchableOpacity
+              style={styles.Frow}
+              onPress={() => this.props.navigation.navigate('Bantuan')}>
+              <View style={styles.SubMain}>
+                <View style={styles.TPromo}>
+                  <Text style={styles.TText}>Lacak Pesanan</Text>
+                  <Text style={{ fontSize: 13 }}>Melacak Order pesananmu</Text>
                 </View>
-                <View style={styles.arr}>
-                <Image source={require('../../assets/img/next.png')} style={styles.imgArr} />
-                </View>
+              </View>
+              <View style={styles.arr}>
+                <Image
+                  source={require('../../assets/img/next.png')}
+                  style={styles.imgArr}
+                />
+              </View>
             </TouchableOpacity>
-            
-        </View>
-        <View style={styles.Main}>
-             <TouchableOpacity style={styles.Frow} onPress={() => this.props.navigation.navigate('Ulasan')}>
-                <View style={styles.SubMain}>
-                    
-                    <View style={styles.TPromo}>
-                        <Text style={styles.TText}>Ulasan Pelanggan</Text>
-                        <Text style={{fontSize:13}}>Berikan penilaian dan ulasan produk</Text>
-                    </View>
+          </View>
+          <View style={styles.Main}>
+            <TouchableOpacity
+              style={styles.Frow}
+              onPress={() => this.props.navigation.navigate('Ulasan')}>
+              <View style={styles.SubMain}>
+                <View style={styles.TPromo}>
+                  <Text style={styles.TText}>Ulasan Pelanggan</Text>
+                  <Text style={{ fontSize: 13 }}>
+                    Berikan penilaian dan ulasan produk
+                  </Text>
                 </View>
-                <View style={styles.arr} >
-                        <Image source={require('../../assets/img/next.png')} style={styles.imgArr} />
-                    </View>
+              </View>
+              <View style={styles.arr}>
+                <Image
+                  source={require('../../assets/img/next.png')}
+                  style={styles.imgArr}
+                />
+              </View>
             </TouchableOpacity>
-            
-        </View>
-        <Button block onPress={ this.logoutHandler } style={{marginTop: 30, marginLeft: 10, marginRight: 10, backgroundColor: '#009975' }}>
-            <Text style={{color:'#ffffff', fontSize: 17}}>Logout</Text>
-        </Button>
-        
-
+          </View>
+          <Button
+            bordered
+            success
+            style={{
+              marginTop: 16,
+              borderRadius: 8,
+              width: '80%',
+              alignSelf: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text>Download Katalog Produk</Text>
+          </Button>
+          <Button
+            bordered
+            success
+            onPress={this.logoutHandler}
+            style={{
+              marginTop: 16,
+              borderRadius: 8,
+              width: '80%',
+              alignSelf: 'center',
+              justifyContent: 'center',
+            }}>
+            <Text>Logout</Text>
+          </Button>
         </Content>
       </Container>
     );
@@ -136,38 +197,37 @@ export default class CardImageExample extends Component {
 
 const styles = StyleSheet.create({
   Main: {
-      flex: 1,
-      marginTop: 20,
+    flex: 1,
   },
   SubMain: {
-      flexDirection: 'row',
-      alignItems: 'center',
-      width: '80%',
-      marginBottom: 10
+    flexDirection: 'row',
+    alignItems: 'center',
+    width: '80%',
+    marginBottom: 10,
   },
   img: {
-      height: 25,
-      width: 25
+    height: 25,
+    width: 25,
   },
   TPromo: {
-      marginLeft: 10,
+    marginLeft: 10,
   },
   TText: {
-      fontSize: 15
+    fontSize: 15,
   },
   arr: {
-      alignItems: 'flex-start',
-      justifyContent: 'center',
-      marginLeft: 25,
+    alignItems: 'flex-start',
+    justifyContent: 'center',
+    marginLeft: 25,
   },
   Frow: {
-      flexDirection: 'row',
-      borderBottomWidth: 1,
-      margin: 10,
-      borderBottomColor:'#eee',
+    flexDirection: 'row',
+    borderBottomWidth: 1,
+    margin: 10,
+    borderBottomColor: '#eee',
   },
   imgArr: {
-      height: 10,
-      width: 10
-  }
-})
+    height: 10,
+    width: 10,
+  },
+});
